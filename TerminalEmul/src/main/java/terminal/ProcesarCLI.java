de4c6fae5803b51dc7b560dd1ab906809294fe40
root <root@linuxserver>
2019-09-18 22:26:46 -0300

    Terminal casi funcional

--- TerminalEmul/src/main/java/terminal/ProcesarCLI.java
+++ TerminalEmul/src/main/java/terminal/ProcesarCLI.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package terminal;
 
-/**
- *
- * @author root
- */
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class ProcesarCLI {
-    
+
+    private String textoCLI;
+    private CommandLineParser parser = null;
+    private CommandLine cmdLine = null;
+    private Options options;
+
+    ProcesarCLI(String lineaTextoIngresada) {
+        try {
+            parser = new BasicParser();
+            cmdLine = parser.parse(options, lineaTextoIngresada.split(" "));
+            
+            // Si está la opcion de ayuda, la imprimimos y salimos.
+            if (cmdLine.hasOption("h")){    // No hace falta preguntar por el parámetro "help". Ambos son sinónimos
+                new HelpFormatter().printHelp(CliApp.class.getCanonicalName(), options );
+                return;  
+            }
+        } catch (ParseException ex) {
+            Logger.getLogger(ProcesarCLI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ 
+    }
+
+    public String ejecutar() {
+        String textoSalida = "";
+
+        return textoSalida;
+    }
+
 }
